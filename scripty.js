d6a970fbff86e78370c82f3428306ba23f42a785
keroropata <keroropata@gmail.com>
2020-04-16 11:37:00 -0500

    Add files via upload

---
+++ scripty.js
@@ -0,0 +1,19 @@
+/** created by seth 2019-12-4 **/
+
+$(document).ready(function(){
+    console.log("loaded");
+
+    $(document).on("submit", "#register-form", function (e){
+        e.preventDefault();
+
+        var form = $('#register-form').serialize();
+        $.ajax({
+            url: '/postregistration',
+            type: 'POST',
+            data: form,
+            success: function(response){
+                console.log(response);
+            }
+        });
+    });
+});
